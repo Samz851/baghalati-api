aead7a8abdfd5300d73b67fc2734bcc08d00ea29
Samer Alotaibi <sam.otb@hotmail.ca>
2019-08-15 14:09:28 -0400

    init

---
+++ server/database.js
@@ -0,0 +1,30 @@
+/**
+* Name: database
+*
+* @author Samer Alotaibi
+*		  sam@samiscoding.com
+*
+*
+*
+* Description: mongo database connection
+*
+* Requirements: monggose
+*
+* @package commerce
+* @property
+*
+* @version 1.0
+*/
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost:27017/app-commerce';
+
+mongoose.connect(URI, {
+    useNewUrlParser: true
+})
+    .then(db => console.log(`${URI} is connected`))
+    .catch( err => console.log(`Error en Mongo: ${err}`));
+
+module.exports = mongoose;
+
+
---
+++ server/routes/suppliers.routes.js
@@ -0,0 +1,36 @@
+/**
+* Name: suppliers.routes
+*
+* @author Samer Alotaibi
+*		  sam@samiscoding.com
+*
+*
+* Description: routes for Suppliers
+*
+* Requirements: express
+*
+* @package
+* @property
+*
+* @version 1.0
+*/
+
+const express = require('express');
+const router = express.Router();
+
+const suppliersController = require('../controllers/suppliers.controller');
+
+router.get('/', suppliersController.getSuppliers);
+router.get('/:id', suppliersController.getSupplier);
+
+router.post('/', suppliersController.createSupplier);
+
+router.delete('/:id', suppliersController.deleteSupplier);
+
+router.put('/:id', suppliersController.editSupplier);
+
+module.exports = router;
+
+/** this ends this file
+* server/routes/suppliers.routes
+**/
